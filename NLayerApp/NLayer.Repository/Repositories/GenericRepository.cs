56d3775b531da7baf8e93f978c3827cd942c880e
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2024-05-06 19:04:54 +0300

    create GenericRepository

---
+++ NLayerApp/NLayer.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using System.Linq.Expressions;
+
+namespace NLayer.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            // Efcore AsNoTracking ile çekmiş oldugu dataları memory'e almasın da daha performanslı çalışsın diye.
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+            //_context.Entry(entity).State = EntityState.Deleted; aşağıdaki kod ile aynı işi yapar.Silme işlemi yapmaz sadece state değerini değiştirir.O yüzden async yapılmadı.
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}

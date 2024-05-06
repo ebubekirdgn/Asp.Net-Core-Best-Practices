ca2ecd185d28ff54f60d55db348c646bea93bc27
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2024-05-06 13:51:19 +0300

    create Entity model

---
+++ NLayerApp/NLayer.Core/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace NLayer.Core
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        // Navigation Property
+        public ICollection<Product> Products { get; set; }
+
+    }
+}

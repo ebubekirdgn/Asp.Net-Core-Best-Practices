d8c7bdb85e2154c01dcc43d39644c7fc526336ac
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2024-05-06 19:08:36 +0300

    create UnitOfWork

---
+++ NLayerApp/NLayer.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using NLayer.Core.UnitOfWorks;
+
+namespace NLayer.Repository.UnitOfWork
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}

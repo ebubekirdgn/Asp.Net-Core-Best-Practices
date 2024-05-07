4ade6221cd1920ab9d6ce9274c31da8f77067ea2
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2024-05-07 14:58:29 +0300

    create Dto classes

--- NLayerApp/NLayer.Core/DTOs/ProductDto.cs
+++ NLayerApp/NLayer.Core/DTOs/ProductDto.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NLayer.Core.DTOs
+﻿namespace NLayer.Core.DTOs
 {
-    internal class ProductDto
+    public class ProductDto: BaseDto
     {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
     }
 }
